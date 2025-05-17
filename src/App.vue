6001356b0a082a974ea72edc97d7b47170c6d52e
Your Name <your.email@example.com>
2025-05-17 15:56:41 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { marked } from "marked";
+import { onMounted, ref } from "vue";
+const markdownHtml = ref();
+const hasValidTextContent = ref(true);
+
+onMounted(() => loadMarkdownFile());
+
+async function loadMarkdownFile() {
+  try {
+    const response = await fetch("../src/assets/cmscontent/index.md");
+    if (response.ok) {
+      const markdownText = await response.text();
+      hasValidTextContent.value = !markdownText.includes("!doctype");
+      if (hasValidTextContent.value) markdownHtml.value = marked(markdownText);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+</script>
+
+<template>
+  <div v-if="!hasValidTextContent">Loading.....</div>
+  <div v-else v-html="markdownHtml" />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
